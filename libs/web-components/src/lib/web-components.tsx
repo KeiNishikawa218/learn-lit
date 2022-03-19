8019dc30904fb1c0f7035cd7de1eb54e3b5f23f6
k-nishikawa <k-nishikawa@atware.co.jp>
2022-03-19 21:18:08 +0900

    feat: add web-componet and lit

---
+++ libs/web-components/src/lib/web-components.tsx
@@ -0,0 +1,14 @@
+import './web-components.module.css';
+
+/* eslint-disable-next-line */
+export interface WebComponentsProps {}
+
+export function WebComponents(props: WebComponentsProps) {
+  return (
+    <div>
+      <h1>Welcome to WebComponents!</h1>
+    </div>
+  );
+}
+
+export default WebComponents;
